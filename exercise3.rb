080ab974ed44aebd6266d2202bd80457d88fba90
Vincent Jim <vjim323@gmail.com>
2016-04-28 16:49:25 -0400

    prompts user for their age and tells them what year they were born in

--- exercise3.rb
+++ exercise3.rb
@@ -3,3 +3,13 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Hi #{name}!"
+
+puts "How old are you?"
+
+age = gets.to_i
+
+currentyear = 2016
+
+birthyear = currentyear - age
+
+puts "Ah okay, so you were born in #{birthyear}."
